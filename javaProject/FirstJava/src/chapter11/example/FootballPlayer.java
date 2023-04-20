a3faa8277114955c1338da220db1f4e7f35da6cc
jissssoo1717 <jisoolee7101@gmail.com>
2023-04-20 23:22:17 +0900

    .

--- javaProject/FirstJava/src/chapter11/example/FootballPlayer.java
+++ javaProject/FirstJava/src/chapter11/example/FootballPlayer.java
@@ -1,14 +1,10 @@
 package chapter11.example;
 
 public class FootballPlayer {
-	
+
 	String name;
 	int number;
 	String team;
 	int age;
-	
-	public static void main(String[] args) {
-		List<FootballPlayer> footballPlayerInfo = new ArrayList<>();
-	}
 
 }
